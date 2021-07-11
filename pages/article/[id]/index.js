ebdde55eb993efa09f9b036db15200d0c5e3ae24
laniekea <jzkoay@gmail.com>
2021-07-11 22:54:42 +0800

    add getserversideprops in article page, and put link to go back home.

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,7 +1,30 @@
-const article = () => {
-    return (
-        <div>This is an article</div>
-    )
-}
+import { useRouter } from "next/router";
+import Link from "next/link";
 
-export default article
+const article = ({ article }) => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <o>{article.body}</o>
+      <br />
+      <Link href="/">Go Back</Link>
+    </>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+
+  const article = await res.json();
+
+  return {
+    props: { article },
+  };
+};
+
+export default article;
